673060dce680eb95c7827874480c25f71a86c7a6
João <joaogui.silvae@gmail.com>
2026-03-10 08:51:29 -0300

    chore: add shadcn/ui setup

---
+++ app/_lib/auth-client.ts
@@ -0,0 +1,10 @@
+import { google } from "better-auth";
+import { createAuthClient } from "better-auth/react";
+
+export const authClient = createAuthClient({
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
+});
+
+authClient.signIn.social({
+  provider: "google",
+});
